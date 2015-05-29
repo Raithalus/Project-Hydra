d1343c907c522c107e56941ff3076cf9690f921a
Dave Cheung <david.cy.cheung@gmail.com>
2015-05-29 01:22:26 -0400

    scrapy startproject

---
+++ Hydra/Hydra/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for Hydra project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'Hydra'
+
+SPIDER_MODULES = ['Hydra.spiders']
+NEWSPIDER_MODULE = 'Hydra.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'Hydra (+http://www.yourdomain.com)'
